6a2340378ddf04de8f49bde6d39a0390edee8c8c
Pavel Koch <kouks.koch@gmail.com>
2017-11-18 12:24:54 +0000

    Page transitions and scss cleanup.

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,18 @@ export default {
 </script>
 
 <style lang="scss">
-  @import 'assets/scss/app';
+@import 'assets/scss/app';
+
+.fade-enter-active, .fade-leave-active {
+  transition-property: opacity;
+  transition-duration: .3s;
+}
+
+.fade-enter-active {
+  transition-delay: .3s;
+}
+
+.fade-enter, .fade-leave-active {
+  opacity: 0
+}
 </style>
