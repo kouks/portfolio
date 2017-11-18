15158324cec7e4242a9f30f64f26d908139975db
Pavel Koch <kouks.koch@gmail.com>
2017-11-18 01:33:15 +0000

    Edited linkedin link

--- src/components/partials/Social.vue
+++ src/components/partials/Social.vue
@@ -1,6 +1,6 @@
 <template>
   <ul>
-    <li><a target="_blank" href="https://www.linkedin.com/in/pavel-koch-25518b129/">
+    <li><a target="_blank" href="https://www.linkedin.com/in/pavel-koch/">
       <i class="fa fa-linkedin fa-fw" aria-hidden="true"></i>
     </a></li>
     <li><a target="_blank" href="http://github.com/kouks">
